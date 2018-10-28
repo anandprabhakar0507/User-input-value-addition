719f7df95b034183f71c8bb9576d31d6fd67bd82
Anand prabhakar <anandprabhakar0507@gmail.com>
2018-10-28 10:17:21 -0700

    Create input add.py

---
+++ input add.py
@@ -0,0 +1,11 @@
+#THIS SCRIPT TAKES INPUT OF NUMBERS and ADD THEM
+
+# Store input numbers
+A = input('')
+B = input('')
+print('YOU JUST ENTERED THE VALUE OF A={0} and B={1}'.format(A, B))
+# Add two numbers
+sum = float(A) + float(B)
+
+# Display the sum
+print('The sum of A and B or A+B is {2}'.format(A, B, sum))
